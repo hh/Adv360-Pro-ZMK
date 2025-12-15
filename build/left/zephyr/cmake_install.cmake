099617d3064e85ddfea84201b71a819226964f47
Hippie Hacker <hh@ii.coop>
2025-12-15 09:59:01 -0500

    feat: Enable HOGP trackpad/mouse relay with debug config
    - Enable CONFIG_ZMK_HOGP with mouse and iTrack output
    - Add dual USB HID interfaces for trackpad separation
    - Enable BLE central role and MTU negotiation
    - Add serial command support
    - Update keymap with BLE profile bindings

---
+++ build/left/zephyr/cmake_install.cmake
@@ -0,0 +1,98 @@
+# Install script for directory: /var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/zephyr
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set path to fallback-tool for dependency-resolution.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/opt/zephyr-sdk-0.16.9/arm-zephyr-eabi/bin/arm-zephyr-eabi-objdump")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/arch/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/lib/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/soc/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/boards/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/subsys/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/drivers/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/zmk-hogp/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/nanopb/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/acpica/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/canopennode/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/cmsis/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/cmsis-dsp/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/cmsis-nn/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/fatfs/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_ambiq/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/atmel/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_espressif/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_ethos_u/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_gigadevice/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_intel/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_nordic/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/nuvoton/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/quicklogic/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_renesas/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_rpi_pico/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/stm32/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_telink/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/hal_wurthelektronik/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/libmetal/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/liblc3/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/littlefs/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/lvgl/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/mbedtls/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/mipi-sys-t/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/nrf_hw_models/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/open-amp/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/percepio/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/picolibc/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/segger/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/tinycrypt/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/trusted-firmware-a/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/uoscore-uedhoc/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/zcbor/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/modules/module/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/kernel/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/cmake/flash/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/cmake/usage/cmake_install.cmake")
+  include("/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/cmake/reports/cmake_install.cmake")
+
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/var/home/hh/w/touchpad/src/Adv360-Pro-ZMK/build/left/zephyr/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
